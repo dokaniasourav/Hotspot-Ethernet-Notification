b4a591d746b0b5e078dd2189c13c59c7d315a5ef
Sourav Dokania <sourav.dokania25@gmail.com>
2020-12-19 14:22:21 +0530

    Add files via upload

---
+++ Hosted_Network.ps1
@@ -0,0 +1,73 @@
+﻿$a = netsh wlan show hostednetwork
+$messege = ""
+$table = @{"c8:25:e1:22:3f:d7" = "Le 1S, Sourav Dokania";
+           "00:00:00:00:00:00" = "Invalid";
+           "90:48:9a:42:c4:45" = "HP Laptop, Ayush Aggarwal";
+           "10:08:b1:9f:f4:9d" = "Lenovo, Abhijit Tripathy";
+           "ec:88:92:78:8b:1a" = "Moto G2, Ayush Aggarwal";
+		   "5c:93:a2:df:ce:cf" = "Lenovo, Chirag Mehta"
+		   }
+$status = !$a[11].Contains("Not started")
+if($status)
+{   
+    $b = "Number of clients      :"
+    $NumberOfClients = $a[15].Substring($a[15].IndexOf($b) + $b.Length + 1).ToInt32($null)
+    #Write-Host "No of clients = $NumberOfClients"
+        $clients = @(0) * $NumberOfClients
+        #Write-Host $NumberOfClients
+        for($i = 0; $i -lt $NumberOfClients; $i++)
+        {
+            $clients[$i] = $a[16+$i].Substring(8,17)
+            #write-host $clients[$i]
+            if($table.ContainsKey($clients[$i]))
+            {
+                $messege += $table.Get_Item($clients[$i]) + "`n"
+                
+            }
+            else
+            {
+                $messege += $clients[$i] + "`n"
+            }
+            Write-Host $messege
+        }
+}
+else
+{
+    #write-host "HosSpot Disabled"
+}
+
+<#function macResolve([String]$macAddress)
+{
+    if($Global:table.ContainsKey($macAddress))
+    {
+        $Global:table.Get_Item($macAddress)
+    }
+    else
+    {
+        $macAddress
+    }
+}#>
+if($NumberOfClients -eq 0) {$messege = "None Connected"}
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+$objNotifyIcon = New-Object System.Windows.Forms.NotifyIcon 
+$objNotifyIcon.Icon = "C:\Users\sourav\Desktop\devil.ico"
+$objNotifyIcon.BalloonTipIcon = "Info"
+$objNotifyIcon.BalloonTipText = $messege
+$objNotifyIcon.BalloonTipTitle = "Devices Connected`n" + [DateTime]::Now.ToLongDateString()
+$objNotifyIcon.Visible = $True
+$objNotifyIcon.ShowBalloonTip(2000)
+
+<#
+Write-Host "`r`nPRESS Ctrl+C   OR WAIT FOR..."
+$timeout = new-timespan -Seconds 6
+$sw = [diagnostics.stopwatch]::StartNew()
+$t = 5
+while ($sw.elapsed -lt $timeout){
+    start-sleep -seconds 1
+    Write-Host $t "Seconds"
+    $t = $t - 1
+}
+while(1)
+{
+}
+<##>
---
+++ NetworkProxyModifier.ps1
@@ -0,0 +1,101 @@
+﻿$messege = ""
+
+
+
+#Function to see what proxy needs to be set
+
+    $regKey="HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+    [String]$proxyServerMorning = "localhost:9001"
+    [String]$proxyServerEvening = "172.16.15.11:8080"
+    $proxyServer = Get-ItemProperty -path $regKey -ErrorAction SilentlyContinue
+    $day =  Get-Date
+    if(($day.DayOfWeek.value__ -ne 6) -and 
+       ($day.DayOfWeek.value__ -ne 0) -and
+       ($day.Hour -gt 9) -and ($day.Hour -lt 19))
+       {
+            [String]$proxyValue = $proxyServerMorning
+       }
+       else
+       {
+            [String]$proxyValue = $proxyServerEvening
+       }
+
+
+#Function to return true if ethernet connected
+    $networkAdapters = Get-NetAdapter -Name "Ethernet" -ErrorAction SilentlyContinue
+    if($networkAdapters.MediaConnectionState -eq "Connected")
+    {
+        [Int]$proxyEnable = 1
+    }
+    else
+    {
+        [Int]$proxyEnable = 0
+    }
+
+
+    $regKey="HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+    $currentSettings = Get-ItemProperty -Path $regKey
+    if($currentSettings.ProxyEnable -ne $proxyEnable)
+    {
+        Set-ItemProperty -Path $regKey -Name ProxyEnable -Value $proxyEnable
+        $messege += If($proxyEnable){"Ethernet Inserted, Proxy Enabled`n"} else {"Lan Wire Removed, Proxy Disabled`n"}
+    }
+    if($currentSettings.ProxyServer -ne $proxyValue)
+    {
+        Set-ItemProperty -Path $regKey -Name ProxyServer -Value $proxyValue
+        $messege += "The Value changed to $proxyValue `n"
+    }
+    #Write-Host ProxyEnabled = ($proxyEnable -eq 1) and Value = $proxyValue
+
+
+
+
+#Sends a System-Wide Braodcast to all applications that the proxy settings have been changed
+
+$source=@"
+[DllImport("wininet.dll")]
+public static extern bool InternetSetOption(int hInternet, int dwOption, int lpBuffer, int dwBufferLength);
+"@
+#Create type from sourc
+$wininet = Add-Type -memberDefinition $source -passthru -name InternetSettings
+#INTERNET_OPTION_PROXY_SETTINGS_CHANGED
+$wininet::InternetSetOption([IntPtr]::Zero, 95, [IntPtr]::Zero, 0)|out-null
+#INTERNET_OPTION_REFRESH
+$wininet::InternetSetOption([IntPtr]::Zero, 37, [IntPtr]::Zero, 0)|out-null
+
+
+    $a = netsh wlan show hostednetwork
+    Write-Host $a[11]
+    if($a[11].Contains("Not started"))
+    {
+        $k = netsh wlan start hostednetwork
+        $messege += $k
+    }
+    else
+    {
+        $b = "Number of clients      :"
+        $NumberOfClients = $a[15].Substring($a[15].IndexOf($b) + $b.Length + 1)
+        $messege += "Hotspot Running, $NumberOfClients Connected"
+    }
+
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+$objNotifyIcon = New-Object System.Windows.Forms.NotifyIcon 
+$objNotifyIcon.Icon = "C:\Users\sourav\Desktop\devil.ico"
+$objNotifyIcon.BalloonTipIcon = "Info"
+$objNotifyIcon.BalloonTipText = $messege
+$objNotifyIcon.BalloonTipTitle = [DateTime]::Now.ToLongDateString()
+$objNotifyIcon.Visible = $True
+$objNotifyIcon.ShowBalloonTip(2000)
+
+<#
+Write-Host "`r`nPRESS Ctrl+C   OR WAIT FOR..."
+$timeout = new-timespan -Seconds 6
+$sw = [diagnostics.stopwatch]::StartNew()
+$t = 5
+while ($sw.elapsed -lt $timeout){
+    start-sleep -seconds 1
+    Write-Host $t "Seconds"
+    $t = $t - 1
+}
+<##>
